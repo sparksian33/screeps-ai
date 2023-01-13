34acc21298d37d3e755a29eb15a78a3269c63079
sparksian33 <sparksian33@gmail.com>
2023-01-13 13:11:33 -0600

    Message

--- main.js
+++ main.js
@@ -1,31 +1,41 @@
 module.exports.loop = function () {
-    //spawnCreep([WORK, MOVE, CARRY, MOVE], "Creep1");
+    
     console.log(`Current game tick is ${Game.time}`);
-    for(const i in Game.spawns) {
-        Game.spawns[i].spawnCreep([WORK, MOVE, CARRY], "Worker" + Game.time);
-        console.log(Game.spawns[i]);
-    }
+    makeWorkerCreeps(Game);
+    const workerNumber = Object.keys(Game.creeps).length;
+    console.log(workerNumber);
 
     for (const i in Game.creeps) {
         // Game.creeps.$i.memory == creep.memory
         const creep = Game.creeps[i];
-        if (creep.store.getFreeCapacity() === 0 || creep.memory.isUpgrading === true) {
-            if (creep.room.controller) {
-                creep.memory.isUpgrading = true;
-                if (creep.store.getUsedCapacity() === 0) {
-                    creep.memory.isUpgrading = false;
-                }
-                if(creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(creep.room.controller);
-                }
-            }
+        if (creep.store.getFreeCapacity() === 0 || creep.memory.isUpgrading) {
+            goToController(creep);
         } else {
-            const target = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-            if (target) {
-                if (creep.harvest(target) === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target);
-                }
-            }
+            goToHarvest(creep);
         }
+    }
+}
+
+function goToController(creep) {
+    if (creep.room.controller) {
+        creep.memory.isUpgrading = (creep.store.getUsedCapacity() > 0);
+        if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE){
+            creep.moveTo(creep.room.controller);
+        }
+    }
+}
+
+function goToHarvest(creep) {
+    const target = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+    if (target) {
+        if (creep.harvest(target) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(target);
+        }
+    }
+}
+
+function makeWorkerCreeps(Game) {
+    for (const i in Game.spawns) {
+        Game.spawns[i].spawnCreep([WORK, MOVE, CARRY], "Worker" + Game.time);
     }
 }

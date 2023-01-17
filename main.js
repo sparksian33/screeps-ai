84b29126a760e35e83cbf2ba8f5bd221dd02d7b5
sparksian33 <sparksian33@gmail.com>
2023-01-17 12:45:12 -0600

    Message

--- main.js
+++ main.js
@@ -6,25 +6,53 @@ module.exports.loop = function () {
     console.log(workerNumber);
 
     for (const i in Game.creeps) {
-        // Game.creeps.$i.memory == creep.memory
+
         const creep = Game.creeps[i];
-        if (creep.store.getFreeCapacity() === 0 || creep.memory.isUpgrading) {
-            goToController(creep);
-        } else {
-            goToHarvest(creep);
+
+        if (creep.memory.role === role.spawnBaby) {
+            goToSpawn(creep)
+        }
+        if (creep.memory.role === role.upgraderCreep) {
+            upgradeRoomController(creep)
         }
     }
 }
 
-function goToController(creep) {
-    if (creep.room.controller) {
+
+const role = {
+    upgraderCreep: "upgrader",
+    builderCreep: "builder",
+    spawnBaby: "spawn baby"
+}
+
+
+function goToSpawn(creep) {
+    if (creep.store.getFreeCapacity() === 0 || creep.memory.isTransfering) {
+        creep.memory.isTransfering = (creep.store.getUsedCapacity() > 0);
+        let spawn = Game.spawns["Spawn1"]
+        if (creep.transfer(spawn, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(spawn);
+        }
+    }
+    else {
+        upgradeRoomController(creep);
+    }
+}
+
+function upgradeRoomController(creep) {
+    if (creep.store.getFreeCapacity() === 0 || creep.memory.isUpgrading) {
         creep.memory.isUpgrading = (creep.store.getUsedCapacity() > 0);
-        if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE){
+        if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
             creep.moveTo(creep.room.controller);
         }
     }
+    else {
+        goToHarvest(creep);
+    }
 }
 
+
+// This function will be the else to (upgradeController, builders, spawnBaby)
 function goToHarvest(creep) {
     const target = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
     if (target) {
@@ -34,8 +62,34 @@ function goToHarvest(creep) {
     }
 }
 
-function makeWorkerCreeps(Game) {
-    for (const i in Game.spawns) {
-        Game.spawns[i].spawnCreep([WORK, MOVE, CARRY], "Worker" + Game.time);
+
+function makeWorkerCreeps() {
+    //make logic in here to figure out which roles
+    for (const name in Game.spawns) {
+        let role = determineRole()
+        Game.spawns[name].spawnCreep([WORK, MOVE, CARRY, MOVE], role + "_" + Game.time, {
+            memory: {role: role}
+        });
     }
+}
+
+
+function determineRole() {
+    let upgraders = getCreepsByRole(role.upgraderCreep).length
+    let spawnBabies = getCreepsByRole(role.spawnBaby).length
+    if (spawnBabies < 2) {
+        return role.spawnBaby
+    }
+    if (upgraders < 10) {
+        return role.upgraderCreep
+    }
+    console.log("Upgraders: " + upgraders)
+    return role.upgraderCreep
+}
+
+
+function getCreepsByRole(role) {
+    return Object.keys(Game.creeps).filter(name => {
+       return name.startsWith(role)
+    })
 }

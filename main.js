6967c51e21bfa3dd2f83b03b0e3eed1901b00323
sparksian33 <sparksian33@gmail.com>
2023-01-26 13:16:53 -0600

    Message

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 module.exports.loop = function () {
-    
-    console.log(`Current game tick is ${Game.time}`);
-    makeWorkerCreeps(Game);
+
+    //    console.log(`Current game tick is ${Game.time}`);
+    cleanUpMemory();
+    makeCreeps(Game);
     const workerNumber = Object.keys(Game.creeps).length;
-    console.log(workerNumber);
+    console.log("Total Creeps: " + workerNumber);
+
+    let spawn = Game.spawns["Spawn1"];
 
     for (const i in Game.creeps) {
 
         const creep = Game.creeps[i];
+        let hasTask = false;
 
-        if (creep.memory.role === role.spawnBaby) {
+        if (creep.memory.role === role.spawnBaby && spawn.store.getFreeCapacity(RESOURCE_ENERGY) !== 0) {
+            hasTask = true;
             goToSpawn(creep)
         }
-        if (creep.memory.role === role.upgraderCreep) {
+        else if (creep.memory.role === role.builderCreep) {
+            // creep.room.visual.circle(creep.pos.x, creep.pos.y, {
+            //     radius: 1,
+            // })
+            hasTask = true;
+            goToBuild(creep)
+        }
+        else if (creep.memory.role === role.attackerCreep) {
+            hasTask = true;
+            goToHostile(creep)
+        }
+
+        if (!hasTask) {
             upgradeRoomController(creep)
         }
     }
@@ -22,7 +39,19 @@ module.exports.loop = function () {
 const role = {
     upgraderCreep: "upgrader",
     builderCreep: "builder",
-    spawnBaby: "spawn baby"
+    spawnBaby: "spawn baby",
+    attackerCreep: "attacker"
+}
+
+
+function goToHostile(creep) {
+    const target = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
+    if (target) {
+        console.log(target)
+        if (creep.rangedAttack(target) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(target);
+        }
+    }
 }
 
 
@@ -39,6 +68,43 @@ function goToSpawn(creep) {
     }
 }
 
+
+function goToBuild(creep) {
+    if (creep.store.getFreeCapacity() === 0 || creep.memory.isBuilding) {
+        creep.memory.isBuilding = (creep.store.getUsedCapacity() > 0);
+        target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+        //        console.log(target)
+        if (target != null) {
+            if (creep.build(target) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(target);
+            }
+        }
+        else if (target == null) {
+            //            console.log(target)
+            //          not efficient. Goes to random extension instead of the closest by Path/Range
+            let isRefilling = false;
+            for (const i in Game.structures) {
+                let structure = Game.structures[i]
+                let allowedStructures = [STRUCTURE_EXTENSION, STRUCTURE_TOWER, STRUCTURE_STORAGE]
+                if (allowedStructures.includes(structure.structureType)  && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
+                    isRefilling = true;
+                    if (creep.transfer(structure, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(structure);
+                    }
+                }
+            }
+            if (!isRefilling) {
+                upgradeRoomController(creep);
+            }
+        }
+    }
+    else {
+        goToHarvest(creep)
+    }
+}
+
+
+// This function will be the default to (builders and spawnBaby)
 function upgradeRoomController(creep) {
     if (creep.store.getFreeCapacity() === 0 || creep.memory.isUpgrading) {
         creep.memory.isUpgrading = (creep.store.getUsedCapacity() > 0);
@@ -52,7 +118,7 @@ function upgradeRoomController(creep) {
 }
 
 
-// This function will be the else to (upgradeController, builders, spawnBaby)
+// This function will be the else to (upgradeRoomController)
 function goToHarvest(creep) {
     const target = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
     if (target) {
@@ -63,33 +129,62 @@ function goToHarvest(creep) {
 }
 
 
-function makeWorkerCreeps() {
+function makeCreeps() {
     //make logic in here to figure out which roles
     for (const name in Game.spawns) {
-        let role = determineRole()
-        Game.spawns[name].spawnCreep([WORK, MOVE, CARRY, MOVE], role + "_" + Game.time, {
-            memory: {role: role}
-        });
+        let role = determineRole(name)
+        let spawn = Game.spawns[name]
+        const targets = spawn.room.find(FIND_HOSTILE_CREEPS)
+        if (targets.length > 0) {
+            Game.spawns[name].spawnCreep([MOVE, MOVE, RANGED_ATTACK], role + "_" + Game.time, {
+                memory: { role: role }
+            });
+        }
+        if (role != null) {
+            Game.spawns[name].spawnCreep([WORK, MOVE, CARRY, MOVE], role + "_" + Game.time, {
+                memory: { role: role }
+            });
+        }
     }
 }
 
 
-function determineRole() {
+function determineRole(name) {
     let upgraders = getCreepsByRole(role.upgraderCreep).length
     let spawnBabies = getCreepsByRole(role.spawnBaby).length
-    if (spawnBabies < 2) {
+    let builders = getCreepsByRole(role.builderCreep).length
+    let attackers = getCreepsByRole(role.attackerCreep).length
+    let targets = Game.spawns[name].room.find(FIND_HOSTILE_CREEPS).length
+    if (targets > 0) {
+        console.log("Attackers: " + attackers)
+        return role.attackerCreep
+    }
+    if (spawnBabies < 10) {
+        console.log("Spawn Babies: " + spawnBabies)
         return role.spawnBaby
     }
-    if (upgraders < 10) {
+    if (builders < 10) {
+        console.log("Builders: " + builders)
+        return role.builderCreep
+    }
+    if (upgraders < 8) {
         return role.upgraderCreep
     }
     console.log("Upgraders: " + upgraders)
-    return role.upgraderCreep
+    return null
 }
 
 
 function getCreepsByRole(role) {
     return Object.keys(Game.creeps).filter(name => {
-       return name.startsWith(role)
+        return name.startsWith(role)
     })
+}
+
+function cleanUpMemory() {
+    for (const i in Memory.creeps) {
+        if (!(i in Game.creeps)) {
+            delete Memory.creeps[i]
+        }
+    }
 }
